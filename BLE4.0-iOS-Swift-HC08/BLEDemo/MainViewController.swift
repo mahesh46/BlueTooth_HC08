b0fc9b0976193c7c45f2c85e094711e6b9da1581
Mahesh Lad <mlad.mahesh.lad@gmail.com>
2019-03-28 12:21:35 +0000

    initial commit

---
+++ BLE4.0-iOS-Swift-HC08/BLEDemo/MainViewController.swift
@@ -0,0 +1,185 @@
+//
+//  MainViewController.swift
+//  CompassCompanion
+//
+//  Created by Mahesh Lad on 21/03/2019.
+//  Copyright © 2019 Mahesh Lad. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class MainViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var manager:CBCentralManager? = nil
+    var mainPeripheral:CBPeripheral? = nil
+    var mainCharacteristic:CBCharacteristic? = nil
+    
+    //for SH-HC-08   bluetooth device these are the service and characteristics required
+    let BLEService = "FFE0"  //UUID  this is for sh-hc-08 bluetooth device
+    let BLECharacteristic = "FFE1"  //Characteristics UUID  this is for sh-hc-08 bluetooth device
+    
+    @IBOutlet weak var recievedMessageText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager = CBCentralManager(delegate: self, queue: nil);
+        
+        customiseNavigationBar()
+    }
+    
+    func customiseNavigationBar () {
+        
+        self.navigationItem.rightBarButtonItem = nil
+        
+        let rightButton = UIButton()
+        
+        if (mainPeripheral == nil) {
+            rightButton.setTitle("Scan", for: [])
+            rightButton.setTitleColor(UIColor.blue, for: [])
+            rightButton.frame = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 60, height: 30))
+            rightButton.addTarget(self, action: #selector(self.scanButtonPressed), for: .touchUpInside)
+        } else {
+            rightButton.setTitle("Disconnect", for: [])
+            rightButton.setTitleColor(UIColor.blue, for: [])
+            rightButton.frame = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 100, height: 30))
+            rightButton.addTarget(self, action: #selector(self.disconnectButtonPressed), for: .touchUpInside)
+        }
+        
+        let rightBarButton = UIBarButtonItem()
+        rightBarButton.customView = rightButton
+        self.navigationItem.rightBarButtonItem = rightBarButton
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if (segue.identifier == "scan-segue") {
+            let scanController : ScanTableViewController = segue.destination as! ScanTableViewController
+            
+            //set the manager's delegate to the scan view so it can call relevant connection methods
+            manager?.delegate = scanController
+            scanController.manager = manager
+            scanController.parentView = self
+        }
+        
+    }
+    
+    // MARK: Button Methods
+    @objc func scanButtonPressed() {
+        performSegue(withIdentifier: "scan-segue", sender: nil)
+    }
+    
+    @objc func disconnectButtonPressed() {
+        //this will call didDisconnectPeripheral, but if any other apps are using the device it will not immediately disconnect
+        manager?.cancelPeripheralConnection(mainPeripheral!)
+    }
+    
+    @IBAction func sendButtonPressed(_ sender: AnyObject) {
+        let asciitext = "H"
+        let dataToSend = asciitext.data(using: String.Encoding.utf8)
+        
+        if (mainPeripheral != nil) {
+            mainPeripheral?.writeValue(dataToSend!, for: mainCharacteristic!, type: CBCharacteristicWriteType.withoutResponse)
+        } else {
+            print("haven't discovered device yet")
+        }
+    }
+    
+    @IBAction func ButtonOffLED(_ sender: Any) {
+        let asciitext = "L"
+        let dataToSend = asciitext.data(using: String.Encoding.utf8)
+        
+        if (mainPeripheral != nil) {
+            mainPeripheral?.writeValue(dataToSend!, for: mainCharacteristic!, type: CBCharacteristicWriteType.withoutResponse)
+        } else {
+            print("haven't discovered device yet")
+        }
+        
+        
+    }
+    
+    
+    // MARK: - CBCentralManagerDelegate Methods    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        mainPeripheral = nil
+        customiseNavigationBar()
+        print("Disconnected" + peripheral.name!)
+    }
+    
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print(central.state)
+    }
+    
+    // MARK: CBPeripheralDelegate Methods
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        
+        for service in peripheral.services! {
+            
+            print("Service found with UUID: " + service.uuid.uuidString)
+            
+            //Bluno Service
+            if (service.uuid.uuidString == BLEService) {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+            
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+
+        //get device name
+        if (service.uuid.uuidString == BLEService) {
+
+            for characteristic in service.characteristics! {
+
+                if (characteristic.uuid.uuidString == BLECharacteristic) {
+                    peripheral.readValue(for: characteristic)
+                    print("Found Device Name Characteristic")
+                }
+
+            }
+
+        }
+
+
+        
+        if (service.uuid.uuidString == BLEService) {
+
+            for characteristic in service.characteristics! {
+
+                if (characteristic.uuid.uuidString == BLECharacteristic) {
+                    //we'll save the reference, we need it to write data
+                    mainCharacteristic = characteristic
+
+                    //Set Notify is useful to read incoming data async
+                    peripheral.setNotifyValue(true, for: characteristic)
+                    print("Found Bluno Data Characteristic")
+                }
+
+            }
+
+        }
+        
+    }
+    
+    fileprivate func recievedMessage(_ characteristic: CBCharacteristic) {
+        //data recieved
+        if(characteristic.value != nil) {
+            let stringValue = String(data: characteristic.value!, encoding: String.Encoding.utf8)!
+            
+            recievedMessageText.text = stringValue
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        
+        if (characteristic.uuid.uuidString == BLECharacteristic) {
+            recievedMessage(characteristic)
+        }
+        
+    }
+    
+}
+
